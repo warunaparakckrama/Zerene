e15abd1304a54f2eab8fe5358049a8467f8f231c
waruna parakckrama <contactmeuz1325@gmail.com>
2023-10-05 23:38:25 +0530

    f1-mvc basic structure
    Initial file structure created.
    feature1 - first push

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+//DB params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'zmvc'); //our dbname
+
+//App root
+define('APPROOT', dirname(dirname(__FILE__)));
+//URL root
+define('URLROOT', 'http://localhost/ZereneMVC'); //folder in htdocs
+//Site name
+define('SITENAME', 'Zerene'); //our sitename
