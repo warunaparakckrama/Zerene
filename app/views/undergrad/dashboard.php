17c1795c19d517d1fb776a20c0be41b314ac364b
waruna parakckrama <contactmeuz1325@gmail.com>
2023-10-19 21:38:07 +0530

    Searchbar and Buttons
    Created Searchbar.
    Created Notifications, Chats buttons.

--- app/views/undergrad/dashboard.php
+++ app/views/undergrad/dashboard.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php echo SITENAME;?></title>
+        <title><?php echo SITENAME;?> | Undergrad</title>
         <link rel="stylesheet" href="<?php echo CSS; ?>main.css">
         <link rel="stylesheet" href="<?php echo CSS; ?>undergrad.css">
         <link rel="shortcut icon" href="<?php echo IMG;?>favicon.svg" type="image/x-icon">
--- app/views/undergrad/doctors.php
+++ app/views/undergrad/doctors.php
@@ -11,9 +11,8 @@
     <section>
         <div class="grid-1">
             <div class="subgrid-1">
-                <div class="subgrid-2">
-                    <p class="p-title" style="font-size: 40px;">Psychiatrists</p>
-                </div>
+                <div class="subgrid-2"><p class="p-title" style="font-size: 40px;">Psychiatrists</p></div>
+                <div class="subgrid-3"><?php require APPROOT . '/views/inc/searchbar.php';?></div>
             </div>
 
             <div>
