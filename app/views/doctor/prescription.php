3da4b84d0beec8a5765ae7c38c725e44c88f1781
minolijanson <123556458+minolijanson@users.noreply.github.com>
2023-11-02 08:09:15 +0530

    doc_pres new

--- app/views/doctor/prescription.php
+++ app/views/doctor/prescription.php
@@ -6,6 +6,23 @@
         <link rel="stylesheet" href="<?php echo CSS; ?>main.css">
         <link rel="stylesheet" href="<?php echo CSS; ?>dashboard.css">
         <link rel="shortcut icon" href="<?php echo IMG;?>favicon.svg" type="image/x-icon">
+        <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
     </head>
     <body>
         <section>
@@ -20,62 +37,82 @@
                     <div class="card-white">
                         <p class="p-regular"></p>
                         
-                        <div class="card-green">
+                        <div class="card-green-3">
                         <div>
-                        <form action="/action_page.php">
-                        <label for="fname">First Name</label>
-                        <input type="text" id="fname" name="firstname" placeholder="Your name..">
-                        <br>
-
-                        <label for="lname">Last Name</label>
-                        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-                        <input type="submit" value="Submit">
+                        <form class="form-inline" action="/action_page.php">
+                        <label for="email">Name:</label>
+                        <input type="text" id="email" placeholder="undergraduate name" name="email">
+                        <label for="pwd">ID:</label>
+                        <input type="text" id="pwd" placeholder="undergraduate id" name="pswd">
+                        <label for="pwd">date:</label>
+                        <input type="text" id="date" placeholder="date" name="date">
+                        <div class="btn-container">
+                                <button class="button-main">Add</button>
+                            </div>
                         </form>
+                        
                         </div>
                         </div>
                     </div>    
     
-                        
-                        
-    
                     <div class="card-white">
                     <p class="p-regular"></p>
                         
-                    <dev class="card-green">
+                    <dev class="card-green-3">
+                    <form class="form-inline" action="/action_page.php">
+                        <label for="email">medicine:</label>
+                        <input type="text" id="medicine" placeholder="name of medicine" name="medicine">
+                        <label for="pwd">amount(mg):</label>
+                        <input type="text" id="amount" placeholder="amount in mg" name="amount">
+                        <label for="pwd">time:</label>
+                        <input type="text" id="time" placeholder="morning/day/night" name="time">
+                        <div class="btn-container">
+                                <button class="button-main">Add</button>
+                            </div>
+                        </form>
 
-                    <dev class="container">
-                        <div class"row mt-5">
-                            <div class"col-md-3">
-                                <label for ="medecine">medecine</label>
-                                <input type="text" class"form-control" id="medecine">
-                            </div>
-                            <div class"col-md-3">
-                                <label for ="dose">dose   </label>
-                                <input type="text" class"form-control" id="dose">
-                            </div>
-                            <div class"col-md-3">
-                                <label for ="amount">amount(mg)</label>
-                                <input type="text" class"form-control" id="amount(mg)">
-                            </div>
-                            <div class"col-md-3">
-                                <label for ="time">time   </label>
-                                <input type="text" class"form-control" id="time">
-                            </div>
-                        </div>
-                    </dev>
+                   
 
-                     
+                   </div>
+                 <div class="card-white">
+                        <p class="p-regular"></p>
                         
-                             
-                            
-                    </div>
+                        <div class="card-green-3">
+                        <div>
+                        <table>
+                           <tr>
+                           <th>medicine</th>
+                           <th>dose</th>
+                           <th>time</th>
+                           <th></th>
+                           <th></th>
+                           </tr>
+                           <tr>
+                           <td>abc</td>
+                           <td>50mg</td>
+                           <td>night</td>
+                           <td><button class="button-main">edit</button></td>
+                           <td><button class="button-main">delete</button></td>
+                           </tr>
+                           <tr>
+                           <td>pqr</td>
+                           <td>20mg</td>
+                           <td>day and night</td>
+                           <td><button class="button-main">edit</button></td>
+                           <td><button class="button-main">delete</button></td>
+                          </tr>
+
+                        </table>
+                        
 
 
                         </div>
-                    
-                </div>
+                        </div>
+                    </div>    
+               
 
-            </div>
+                 </div>
+            </div>    
 
         </section>
     </body>
