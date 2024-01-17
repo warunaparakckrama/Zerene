6f006023ae46df54c3777b1696e19e66abb676e2
waruna parakckrama <contactmeuz1325@gmail.com>
2024-01-17 08:36:45 +0530

    Minor changes

--- app/views/admin/support_view.php
+++ app/views/admin/support_view.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" href="<?php echo CSS; ?>main.css">
         <link rel="stylesheet" href="<?php echo CSS; ?>dashboard.css">
         <link rel="shortcut icon" href="<?php echo IMG;?>favicon.svg" type="image/x-icon">
-        <title><?php echo SITENAME;?> | Notifications</title>
+        <title><?php echo SITENAME;?> | Support</title>
     </head>
 
     <body>
