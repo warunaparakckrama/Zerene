6d0cf30d02b81c44b1e4e47d6f6c86c20f441fe3
waruna parakckrama <contactmeuz1325@gmail.com>
2024-01-24 09:55:55 +0530

    Username Password Error Handling

--- app/views/admin/ad_profile.php
+++ app/views/admin/ad_profile.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="<?php echo CSS; ?>main.css">
     <link rel="stylesheet" href="<?php echo CSS; ?>dashboard.css">
     <link rel="shortcut icon" href="<?php echo IMG;?>favicon.svg" type="image/x-icon">
+    <title><?php echo $_SESSION['user_name'];?> | Profile</title>
 </head>
 <body>
     <section class="sec-1">
@@ -47,29 +48,29 @@
 
                         <div class="rectangle">
                             <p>Change Password</p>
-                            <form action="<?php echo URLROOT;?>Admin/changePwdAdmin/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
+                            <form name="changePwdAdmin" action="<?php echo URLROOT;?>Admin/changePwdAdmin/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
                                 <label for="fname" class="p-regular-grey">Current Password :</label>
                                 <input type="password" id="current_password" name="current_password" class="form-default">
-                                <p class="p-error"><?php echo $data['current_password_err']; ?></p><br>
+                                <p class="p-error"><?php isset($data['current_password_err']) ? $data['current_password_err'] : ''; ?></p><br>
                                 <label for="fname" class="p-regular-grey">New Password :</label>
                                 <input type="password" id="new_password" name="new_password" class="form-default">
-                                <p class="p-error"><?php echo $data['new_password_err']; ?></p><br>
+                                <p class="p-error"><?php isset($data['new_password_err']) ? $data['new_password_err'] : ''; ?></p><br>
                                 <label for="fname" class="p-regular-grey">Confirm Password :</label>
                                 <input type="password" id="confirm_password" name="confirm_password" placeholder="" class="form-default">
-                                <p class="p-error"><?php echo $data['confirm_password_err']; ?></p><br>
+                                <p class="p-error"><?php echo isset($data['confirm_password_err']) ? $data['confirm_password_err'] : ''; ?></p><br>
                                 <button class="button-main" type="submit">Change</button>
                             </form>
                         </div>
 
                         <div class="rectangle">
                             <p>Change Username</p>
-                            <form action="<?php echo URLROOT;?>Admin/changeUsernameAdmin/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
+                            <form name="changeUsernameAdmin" action="<?php echo URLROOT;?>Admin/changeUsernameAdmin/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
                                 <label for="cusername" class="p-regular-grey">Current Username :</label>
                                 <input type="text" id="current_username" name="current_username" class="form-default">
-                                <p class="p-error"><?php echo $data['current_username_err']; ?></p><br>
+                                <p class="p-error"><?php echo isset($data['current_username_err']) ? $data['current_username_err'] : ''; ?></p><br>
                                 <label for="nusername" class="p-regular-grey">New Username :</label>
                                 <input type="text" id="new_username" name="new_username" class="form-default">
-                                <p class="p-error"><?php echo $data['new_username_err']; ?></p><br>
+                                <p class="p-error"><?php echo isset($data['new_username_err']) ? $data['new_username_err'] : ''; ?></p><br>
                                 <button class="button-main" type="submit">Change</button>
                             </form>
                         </div>
