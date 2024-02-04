b934a98c41b77c7f308e7d699f427e698adf3201
minolijanson <123556458+minolijanson@users.noreply.github.com>
2024-02-04 11:31:43 +0530

    timeslot

--- app/views/doctor/doc_timeslots.php
+++ app/views/doctor/doc_timeslots.php
@@ -24,7 +24,7 @@
                     <p class="p-regular">Create New </p>
                         <div class="sub-container7">
                             <div>
-                                <form action="/action_page.php" methord="post">
+                                <form action="<?php echo URLROOT;?>doctor/addTimeslots/<?php echo $user_id=$_SESSION['user_id'];?>"methord="post">
                                     <label for="slot_date" style="color:grey;">Date : </label>
                                     <input type="date" id="Date" name="slot_date" class="dropbtn">
                                     <label for="slot_time" style="color:grey;">Time : </label>
@@ -36,19 +36,67 @@
                                 </form>
                             </div>
                             <div class="subcontainer6">
-                                <a href="<?php echo URLROOT;?>Admin/notifications_view/<?php echo $notifications->notification_id;?>" ><button class="button-main" type="submit">Create</button></a>
+                                <button class="button-main" type="submit">Create</button>
                                 <button class="button-main" type="reset">Cancel</button>    
                                 
                             </div>
                         </div>
                     
                 </div><br>
+
+               <div class="card-white">
+                    <p class="p-regular">Created</p>
+                    <?php
+                    // Grouping timeslots by date
+                    $groupedTimeslots = [];
+                    foreach ($data['timeslot'] as $timeslot) {
+                        $date = date('l', strtotime($timeslot->slot_date)); // Get the day name (e.g., Monday)
+                        $formattedDate = date('Y-m-d', strtotime($timeslot->slot_date));
+                        $start_time = date('h:ia', strtotime($timeslot->slot_start));
+                        $end_time = date('h:ia', strtotime($timeslot->slot_finish));
+                        $formattedTimeRange = "$start_time - $end_time";
+                        // $time = date('h:ia', strtotime($timeslot->slot_time));
+                        $groupedTimeslots[$formattedDate][$date][] = $formattedTimeRange;
+                    }
+
+                    // Displaying grouped timeslots
+                    foreach ($groupedTimeslots as $formattedDate => $days) {
+                        echo "<div class='card-green-2'>";
+                        foreach ($days as $day => $timeRanges) {
+                            echo "<div>";
+                            echo "<p class='p-regular-grey' style='font-size: 20px;'>$day</p>";
+                            echo "<p class='p-regular-grey' style='font-size: 15px;'>$formattedDate</p>";
+                            echo "</div>";
+                            echo "<div class='btn-container-2'>";
+                            foreach ($timeRanges as $timeRange) {
+                                echo "<button class='button-main'>$timeRange</button>";
+                            }
+                            echo "</div>";
+                        }
+                        echo "</div>";
+                    }
+                    ?>
+                </div>
+
                 <div class="card-white">
-                    <p class="p-regular">Available</p>
+                    <p class="p-regular">Reserved</p>
+                    <div class="card-green-2">
+                        <div>
+                            <p class="p-regular-grey" style="font-size: 20px;">Monday</p>
+                            <p class="p-regular-grey" style="font-size: 15px;">2024-01-29</p>
+                        </div>
+                        <div class="btn-container-2">
+                            <button class="button-second">2.00-2.30pm</button>
+                            <button class="button-second">2.00-2.30pm</button>
+                            <button class="button-second">2.00-2.30pm</button>
+                            <button class="button-second">2.00-2.30pm</button>
+                            <button class="button-second">2.00-2.30pm</button>
+                            <button class="button-second">2.00-2.30pm</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        
+
     </section>
 </body>
-<!-- </html> -->
