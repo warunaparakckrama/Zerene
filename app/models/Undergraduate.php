1f972647bd70f32e59325ae0dcd237187b0cdf90
waruna parakckrama <contactmeuz1325@gmail.com>
2024-02-25 17:07:57 +0530

    Questionnaire Read

--- app/models/Undergraduate.php
+++ app/models/Undergraduate.php
@@ -14,4 +14,47 @@ class Undergraduate
         $results = $this->db->resultSet();
         return $results;
     }
+
+    public function getQuestionnaireDetails()
+    {
+        $this->db->query('SELECT * FROM questionnaires');
+        $results = $this->db->resultSet();
+        return $results;
+    }
+
+    public function getQuestionnairesfromId($questionnaire_id)
+    {
+        $this->db->query('SELECT * FROM questionnaires WHERE questionnaire_id = :questionnaire_id');
+        $this->db->bind(':questionnaire_id', $questionnaire_id);
+        $row = $this->db->single();
+
+        //check row
+        if ($this->db->rowCount() > 0) {
+            return $row;
+        } else {
+            return null;
+        }
+    }
+
+    public function getQuestionsfromQuestionnaireId($questionnaire_id)
+    {
+        $this->db->query('SELECT * FROM question WHERE questionnaire_id = :questionnaire_id');
+        $this->db->bind(':questionnaire_id', $questionnaire_id);
+        $results = $this->db->resultSet();
+        return $results;
+    }
+
+    public function getAnswersfromQuestionnaireId($questionnaire_id)
+    {
+        $this->db->query('SELECT * FROM answer WHERE questionnaire_id = :questionnaire_id');
+        $this->db->bind(':questionnaire_id', $questionnaire_id);
+        $row = $this->db->single();
+        
+        //check row
+        if ($this->db->rowCount() > 0) {
+            return $row;
+        } else {
+            return null;
+        }
+    }
 }
