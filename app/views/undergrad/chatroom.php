dc20ce330cdd1f4a9c800a0c4c3bfd680edd1486
waruna parakckrama <contactmeuz1325@gmail.com>
2024-04-09 18:21:47 +0530

    chatroom build #1
    ratchet folder installed.
    websockets running successfully.
    messages getting sent. (broadcast only)

--- app/views/undergrad/chatroom.php
+++ app/views/undergrad/chatroom.php
@@ -1,4 +1,5 @@
 <?php $currentPage = 'chats'; ?>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,23 +20,122 @@
                 <div class="subgrid-3"><?php require APPROOT . '/views/inc/searchbar.php';?></div>
             </div>
 
-            <div class="subgrid-2">
+            <!-- chatbox section -->
+            <div class="subgrid-2"> 
                 <div class="card-white">
                     
-                    <div class="chat-row" style="border: 1px solid black;">
+                    <div class="" style="border: 1px solid black;">
                         <div class="">
-                            <div class="" id='chat-window'></div>
+                            <div class="" id="chat-window"></div>
                             <div id='typing'></div>
-                            <div id='form' class='row rounded-pill shadow'>
-                                <input class='col-8 rounded-pill  p-1 ps-3  border-0' onkeyup='typing()' id='comment-input' type='text' placeholder='comment'>
-                                <button id='send-button' class='btn btn-primary rounded-pill shadow col-4 ' onclick='send()'>Send</button>
+                            <div id='form' class="">
+                                <input class="" onkeyup="typing()" id="comment-input" type="text" placeholder="enter your message">
+                                <button id="send-button" class="" onclick="send()">Send</button>
                             </div>
                         </div>
                     </div>
 
                 </div>
             </div>
+            <!-- chatbox section -->
 
         </div>
     </section>
+
+   <script>
+        var conn = new WebSocket('ws://localhost:8080');
+        conn.onopen = function(e) {
+            console.log('Connection established!'); 
+            conn.send(JSON.stringify({  
+                'newRoute': 'chat'
+            }));
+
+        };
+
+        let timeoutHandle = window.setTimeout(function(){ 
+            document.getElementById('typing').innerHTML = '';
+        }, 2000);
+
+        conn.onmessage = function(e) {
+            let data = JSON.parse(e.data);
+            console.log(data);
+            if(typeof data.msg !== 'undefined'){
+
+                // document.getElementById('typing').innerHTML = '';
+                // let commentElem = document.createElement('div');
+                // commentElem.classList.add('col-11');
+                // commentElem.classList.add('fill-container');
+                // commentElem.innerHTML = "<div class=''><div class=''>" + data.name + "</div><div class=''>" + data.msg + "</div><div class=''>" +data.date + "</div></div>";
+                
+                var chatWindow = document.getElementById('chat-window');
+
+                var newMessage = document.createElement('p');
+                newMessage.innerHTML = data.name + " : " + data.msg + " " + data.date;
+                newMessage.classList.add();
+                chatWindow.appendChild(newMessage);
+                document.getElementById('chat-window').appendChild(commentElem);
+            }
+            else if(typeof data.typing !== 'undefined'){
+                document.getElementById('typing').innerHTML = data.typing + ' is typing...';
+                window.clearTimeout(timeoutHandle);
+                timeoutHandle = window.setTimeout(function(){ 
+                    document.getElementById('typing').innerHTML = '';
+                }, 2000);
+            }
+        };
+
+        var input = document.getElementById('comment-input');
+        input.addEventListener("keypress", function (event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                document.getElementById("send-button").click();
+            }
+        });
+
+        function typing(){
+            conn.send(JSON.stringify({  
+                'typing': '<?php echo $_SESSION['user_name'];?>'
+            }));
+        }
+
+        function send(){
+            <?php
+            $datesent = date('M d, Y h.i A');
+            $sender = $_SESSION['user_name'];
+            // $commentor = $_SESSION['Name'];
+            ?>
+            // let msg = document.getElementById('msg').value;
+            conn.send(JSON.stringify({  
+                'msg': input.value,
+                'name': '<?= $sender ?>'
+            }));
+            // document.getElementById('msg').value = '';
+
+            var chatWindow = document.getElementById('chat-window');
+            var newMessage = document.createElement('p');
+            newMessage.classList.add();
+            newMessage.innerHTML = '<?= $sender?>' + " : " + input.value + " (" + '<?= $datesent ?>' + ")";
+            chatWindow.appendChild(newMessage);
+            input.value = '';
+        }
+
+        // send messages to database
+        function sendMessage(comment, room) { 
+            let data = {
+                'message': comment,
+                'roomId': room
+            };
+            fetch('http://localhost/Ratchet-with-chat-room/Main/SendPrivate.php', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json())
+            .then(json => { 
+                console.log(json);
+            });
+        }
+
+   </script>
 </body>
