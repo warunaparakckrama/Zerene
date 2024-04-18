016aa7fa3c1ecc69eff3d19024ce77bbef3bfa38
minolijanson <123556458+minolijanson@users.noreply.github.com>
2024-04-18 14:50:26 +0530

    profile

--- app/controllers/Doctor.php
+++ app/controllers/Doctor.php
@@ -77,147 +77,137 @@ class Doctor extends Controller{
         $this->view('doctor/doc_timeslots', $data);
     }
     
-    public function doc_profile(){
-        $data = [];
+    public function doc_profile($user_id){
+        $doctor = $this->adminModel->getDoctorById($user_id);
+        $data = [
+            'doctor' => $doctor
+        ];
         $this->view('doctor/doc_profile', $data);
     }
-    public function changeUsernameDoc($user_id){
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    public function changeUsernameDoc($user_id)
+    {
+    $doctor = $this->adminModel->getUgById($user_id);
+        $current_username = $this->userModel->getUsernameById($user_id);
+        $username = $this->userModel->getUsernames();
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Sanitize POST array
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
             $data = [
-                'current_username' => trim($_POST['current_username']),
+                'doctor' => $doctor,
+                'current_username' => $current_username,
+                'username' => $username,
                 'new_username' => trim($_POST['new_username']),
-                'current_username_err'=>'',
-                'new_username_err'=>''
+                'password' => trim($_POST['password']),
+                'username_alert' => ''
             ];
-        
-            if(empty($data['current_username'])){
-                $data['current_username_err']='Please enter current username';  
-            }
-            if(empty($data['new_username'])){
-                $data['new_username_err']='Please enter new username';  
+
+            if (strlen($data['new_username']) < 8) {
+                $data['username_alert'] = '*Username must be atleast 8 characters';
             }
 
-            if(empty($data['current_username_err']) && empty($data['new_username_err'])){
-                // Validated
-    
-                // Fetch the current username from db
-                $current_username = $this->userModel->getUsernameById($user_id);
-    
-                // Verify if the entered current password matches the hashed password from the database
-                if (($data['current_username'] != $current_username)) {
-                    $data['current_username_err'] = 'Current username is incorrect';
-                } else {
+            elseif($data['new_username'] == $data['current_username']) {
+                $data['username_alert'] = '*New username cannot be same as the current username';
+            }
 
-                    // Update the username
-                    if ($this->userModel->updateUsername($user_id, $data['new_username'])) {
-                    flash('user_message', 'Username updated successfully');
-                    redirect('doctor/doc_profile');
-                    } else {
-                    die('Something went wrong');
+            else {
+                // Convert the new_username to lowercase
+                $newUsernameLower = strtolower($data['new_username']);
+            
+                foreach ($data['username'] as $username) {
+                    // Convert each username in the array to lowercase
+                    $existingUsernameLower = strtolower($username->username);
+                    
+                    // Compare the lowercase versions of the usernames
+                    if ($newUsernameLower === $existingUsernameLower) {
+                        // If there is a match, set the alert message
+                        $data['username_alert'] = '*Username already exists/ is a variation of current username';
+                        break; // Exit the loop as soon as a match is found
                     }
                 }
-
-            } else {
-                // Load view with errors
-                $this->view('doctor/doc_profile', $data);
             }
-        } 
-        
-        else {
-            $data = [
-            'current_username' => '',
-            'new_username' => '',
-            'current_username_err'=>'',
-            'new_username_err'=>''
-          ];
-    
-          $this->view('doctor/doc_profile', $data);
-        }
-
-        $this->view('doctor/doc_profile', $data);
-    }
-
-    public function changePwdDoc($user_id){
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            // Sanitize POST array
-        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-  
-        $data = [
-          'current_password' => trim($_POST['current_password']),
-          'new_password' => trim($_POST['new_password']),
-          'confirm_password' => trim($_POST['confirm_password']),
-          'current_password_err'=>'',
-          'new_password_err'=>'',
-          'confirm_password_err'=>''
-        ];
-
-        if(empty($data['current_password'])){
-            $data['current_password_err']='Please enter current password';      
-        }
-
-        if(empty($data['new_password'])){
-            $data['new_password_err']='Please enter new password';      
-        }elseif(strlen($data['new_password'])<6){
-            $data['new_password_err']='Password must be atleast 6 characters'; 
-        }
-
-        if(empty($data['confirm_password'])){
-            $data['confirm_password_err']='Please re-enter new password';      
-        }else{
-            if($data['new_password']!=$data['confirm_password']){
-                $data['confirm_password_err']='passwords do not match';
-            }
-        }
-
-        
-
-        if(empty($data['username_err']) && empty($data['email_err'])&& empty($data['confirm_password_err'])){
-            // Validated
-
+            
             // Fetch the hashed password from the database based on the user ID
             $hashed_password_from_db = $this->userModel->getPasswordById($user_id);
 
             // Verify if the entered current password matches the hashed password from the database
-            if (!password_verify($data['current_password'], $hashed_password_from_db)) {
-                $data['current_password_err'] = 'Current password is incorrect';
-            } else {
-                // Hash the new password
-                $data['new_password'] = password_hash($data['new_password'], PASSWORD_DEFAULT);
+            if (!password_verify($data['password'], $hashed_password_from_db)) {
+                $data['username_alert'] = '*Incorrect Password';
+            }
 
-                // Update the user's password
-                if ($this->userModel->updatePassword($user_id, $data['new_password'])) {
-                flash('user_message', 'Password updated successfully');
-                redirect('doctor/doc_profile');
+            if (empty($data['username_alert'])) {
+                // Update the username
+                if ($this->userModel->updateUsername($user_id, $data['new_username'])) {
+                    flash('user_message', 'Username updated successfully');
+                    redirect('undergrad/ug_profile'); 
                 } else {
-                die('Something went wrong');
+                    die('Something went wrong');
+                }    
+            } 
+
+            else {
+                // Load view with errors
+                $this->view('doctor/doc_profile', $data);
+            }
+
+        } 
+        
+        $this->view('doctor/doc_profile', $data);
+    }
+
+    public function changePwdDoc($user_id){
+
+        $doctor = $this->adminModel->getUgById($user_id);
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'doctor' => $doctor,
+                'current_password' => trim($_POST['current_password']),
+                'new_password' => trim($_POST['new_password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'password_alert' => '',
+            ];
+
+            if (strlen($data['new_password']) < 8) {
+                $data['alert'] = '*Password must be atleast 8 characters';
+            }
+
+            else {
+                if ($data['new_password'] != $data['confirm_password']) {
+                    $data['alert'] = '*passwords do not match';
                 }
             }
 
-          } else {
-            // Load view with errors
-            $this->view('doctor/doc_profile', $data);
-          }
-        
-        }   
-    
-        else {
-            $data = [
-            'current_password' => '',
-            'new_password' => '',
-            'confirm_password' => '',
-            'current_password_err'=>'',
-            'new_password_err'=>'',
-            'confirm_password_err'=>''
-          ];
-    
-          $this->view('doctor/doc_profile', $data);
+            if (empty($data['password_alert'])) {
+                // Validated
+
+                // Fetch the hashed password from the database based on the user ID
+                $hashed_password_from_db = $this->userModel->getPasswordById($user_id);
+
+                // Verify if the entered current password matches the hashed password from the database
+                if (!password_verify($data['current_password'], $hashed_password_from_db)) {
+                    $data['password_alert'] = '*Current password is incorrect';
+                } else {
+                    // Hash the new password
+                    $data['new_password'] = password_hash($data['new_password'], PASSWORD_DEFAULT);
+
+                    // Update the user's password
+                    if ($this->userModel->updatePassword($user_id, $data['new_password'])) {
+                        flash('user_message', 'Password updated successfully');
+                        redirect('doctor/doc_profile');
+                    } else {
+                        die('Something went wrong');
+                    }
+                }
+            } else {
+                // Load view with errors
+                $this->view('doctor/doc_profile', $data);
+            }
         }
 
-        $this->view('doctor/doc_profile', $data);
-
+        $this->view('undergrad/ug_profile', $data);
     }
 
     public function doc_template(){
