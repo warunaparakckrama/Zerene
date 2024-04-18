627413076c2f0642a0aafaeb78d1e7ca05cb0302
AshanPraboda <111ashanpraboda@gmail.com>
2024-04-18 11:56:02 +0530

    update req let

--- app/views/academic/ac_profile.php
+++ app/views/academic/ac_profile.php
@@ -49,7 +49,7 @@
                             <p>Change Password</p>
                             <form name="changePwdAcademic" action="<?php echo URLROOT;?>Academic/changePwdAcademic/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
                                 <label for="fname" class="p-regular-grey">Current Password :</label>
-                                <input type="password" id="current_password" name="current_password" class="form-default">
+                                <input type="password" id="current_password" name="current_password" class="">
                                 <p class="p-error"><?php echo isset($data['current_password_err']) ? $data['current_password_err'] : ''; ?></p><br>
                                 <label for="fname" class="p-regular-grey">New Password :</label>
                                 <input type="password" id="new_password" name="new_password" class="form-default">
@@ -65,7 +65,7 @@
                             <p>Change Username</p>
                             <form name="changeUsernameAcademic" action ="<?php echo URLROOT;?>Academic/changeUsernameAcademic/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
                                 <label for="cusername" class="p-regular-grey">Current Username :</label>
-                                <input type="text" id="current_username" name="current_username" class="form-default">
+                                <input type="text" id="current_username" name="current_username" class="">
                                 <p class="p-error"><?php echo isset($data['current_username_err']) ? $data['current_username_err'] : ''; ?></p><br>
                                 <label for="nusername" class="p-regular-grey">New Username :</label>
                                 <input type="text" id="new_username" name="new_username" class="form-default">
