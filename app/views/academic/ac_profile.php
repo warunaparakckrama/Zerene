9bba7df63eb689bea6ce2d497a7bcb1d8683da6c
AshanPraboda <111ashanpraboda@gmail.com>
2024-04-18 12:29:38 +0530

    update ac_profile

--- app/views/academic/ac_profile.php
+++ app/views/academic/ac_profile.php
@@ -1,12 +1,14 @@
 <!-- <!DOCTYPE html> -->
 <!-- <html lang="en"> -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo CSS; ?>main.css">
     <link rel="stylesheet" href="<?php echo CSS; ?>dashboard.css">
-    <link rel="shortcut icon" href="<?php echo IMG;?>favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo IMG; ?>favicon.svg" type="image/x-icon">
 </head>
+
 <body>
     <section class="sec-1">
         <div>
@@ -14,8 +16,10 @@
         </div>
         <div class="grid-1">
             <div class="subgrid-1">
-                <div class="subgrid-2"><p class="p-title" style="font-size: 40px;">Profile</p></div>
-                <div class="subgrid-3"><?php require APPROOT . '/views/inc/searchbar.php';?></div>
+                <div class="subgrid-2">
+                    <p class="p-title" style="font-size: 40px;">Profile</p>
+                </div>
+                <div class="subgrid-3"><?php require APPROOT . '/views/inc/searchbar.php'; ?></div>
             </div>
 
             <div>
@@ -30,50 +34,61 @@
                                 <tr>
                                     <td class="p-regular-grey">User ID</td>
                                     <td class="p-regular-grey">:</td>
-                                    <td class="p-title"><?php echo $_SESSION['user_id'];?></td>
+                                    <td class="p-title"><?php echo $_SESSION['user_id']; ?></td>
                                 </tr>
                                 <tr>
                                     <td class="p-regular-grey">Username</td>
                                     <td class="p-regular-grey">:</td>
-                                    <td class="p-title"><?php echo $_SESSION['user_name'];?></td>
+                                    <td class="p-title"><?php echo $_SESSION['user_name']; ?></td>
                                 </tr>
                                 <tr>
                                     <td class="p-regular-grey">E-mail</td>
                                     <td class="p-regular-grey">:</td>
-                                    <td class="p-title"><?php echo $_SESSION['user_email'];?></td>
+                                    <td class="p-title"><?php echo $_SESSION['user_email']; ?></td>
                                 </tr>
                             </table>
                         </div>
 
                         <div class="rectangle">
                             <p>Change Password</p>
-                            <form name="changePwdAcademic" action="<?php echo URLROOT;?>Academic/changePwdAcademic/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
+                            <form name="changePwdAcademic" action="<?php echo URLROOT; ?>Academic/changePwdAcademic/<?php echo $_SESSION['user_id']; ?>" method="POST" class="subgrid-1" style="font-size: 15px;">
                                 <label for="fname" class="p-regular-grey">Current Password :</label>
-                                <input type="password" id="current_password" name="current_password" class="">
-                                <p class="p-error"><?php echo isset($data['current_password_err']) ? $data['current_password_err'] : ''; ?></p><br>
+                                <input type="password" id="current_password" name="current_password" class="" required>
+                                <!-- <p class="p-error"><?php echo isset($data['current_password_err']) ? $data['current_password_err'] : ''; ?></p> -->
+
                                 <label for="fname" class="p-regular-grey">New Password :</label>
-                                <input type="password" id="new_password" name="new_password" class="form-default">
-                                <p class="p-error"><?php echo isset($data['new_password_err']) ? $data['new_password_err'] : ''; ?></p><br>
+                                <input type="password" id="new_password" name="new_password" class="form-default" required>
+                                <!-- <p class="p-error"><?php echo isset($data['new_password_err']) ? $data['new_password_err'] : ''; ?></p> -->
+
                                 <label for="fname" class="p-regular-grey">Confirm Password :</label>
-                                <input type="password" id="confirm_password" name="confirm_password" placeholder="" class="form-default">
-                                <p class="p-error"><?php echo isset($data['confirm_password_err']) ? $data['confirm_password_err'] : ''; ?></p><br>
-                                <button class="button-main" type="submit">Change</button>
+                                <input type="password" id="confirm_password" name="confirm_password" placeholder="" class="form-default" required>
+                                <!-- <p class="p-error"><?php echo isset($data['confirm_password_err']) ? $data['confirm_password_err'] : ''; ?></p> -->
+
+                                <div class="btn-container-2">
+                                    <button class="button-main" type="submit">Change</button>
+                                    <button class="button-danger" type="reset">Cancel</button>
+                                </div>
                             </form>
                         </div>
 
                         <div class="rectangle">
                             <p>Change Username</p>
-                            <form name="changeUsernameAcademic" action ="<?php echo URLROOT;?>Academic/changeUsernameAcademic/<?php echo $_SESSION['user_id'];?>" method="POST" class="subgrid-1">
-                                <label for="cusername" class="p-regular-grey">Current Username :</label>
-                                <input type="text" id="current_username" name="current_username" class="">
-                                <p class="p-error"><?php echo isset($data['current_username_err']) ? $data['current_username_err'] : ''; ?></p><br>
-                                <label for="nusername" class="p-regular-grey">New Username :</label>
-                                <input type="text" id="new_username" name="new_username" class="form-default">
-                                <p class="p-error"><?php echo isset($data['new_username_err']) ? $data['new_username_err'] : ''; ?></p><br>
-                                <button class="button-main" type="submit">Change</button>
+                            <form name="changeUsernameAcademic" action="<?php echo URLROOT; ?>Academic/changeUsernameAcademic/<?php echo $_SESSION['user_id']; ?>" method="POST" class="subgrid-1" style="font-size: 15px;">
+                                <label for="cusername" class="p-regular-grey">New Username :</label>
+                                <input type="text" id="new_username" name="current_username" class="" required>
+                                <!-- <p class="p-error"><?php echo isset($data['current_username_err']) ? $data['current_username_err'] : ''; ?></p> -->
+
+                                <label for="nusername" class="p-regular-grey">Password :</label>
+                                <input type="password"  name="password" required>
+                                <!-- <p class="p-error"><?php echo isset($data['new_username_err']) ? $data['new_username_err'] : ''; ?></p> -->
+
+                                <div class="btn-container-2">
+                                    <button class="button-main" type="submit">Change</button>
+                                    <button class="button-danger" type="reset">Cancel</button>
+                                </div>
                             </form>
                         </div>
-                        
+
                     </div>
                 </div>
 
